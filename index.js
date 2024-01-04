5884997dbdae9d40ea3757c3df66082c1d731c0f
Bikal Rai <bikalrai2785@gmail.com>
2024-01-04 14:25:53 +0545

    add trending, working on top rated

--- index.js
+++ index.js
@@ -5,20 +5,53 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-const API_URL = "https://api.jikan.moe/v4/anime";
+const API_URL = "https://kitsu.io/api/edge";
+const limit = 10;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/", async (req, res) => {
+const getTopRated = async (offset) => {
   try {
-    const response = await axios.get(API_URL);
-    console.log(response.data);
+    const response = await axios.get(
+      `${API_URL}/anime?sort=-averageRating&page[limit]=${limit}&page[offset] = ${offset}`
+    );
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+app.get("/", async (req, res) => {
+  try {
+    const trendingResponse = await axios.get(`${API_URL}/trending/anime`);
+    const topRatedResponse = await axios.get(
+      `${API_URL}/anime?sort=-averageRating`
+    );
+
+    const page = parseInt(req.query.page) || 0;
+    console.log(page);
+
+    const offset = page * limit;
+
+    const trendingAnime = trendingResponse.data.data;
+    const topRatedAnime = await getTopRated(offset);
+
+    res.render("index.ejs", {
+      trending: trendingAnime,
+      topRated: topRatedAnime,
+      page: page,
+      limit: limit,
+    });
   } catch (error) {
     console.log(error);
   }
-
   res.render("index.ejs");
+});
+
+app.get("/top-rated", async (req, res) => {
+  res.render("toprated.ejs");
 });
 
 app.listen(port, () => {
