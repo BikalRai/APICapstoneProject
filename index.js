3ac2dd2f96d6772db54404d437da00c71e04f17e
Bikal Rai <bikalrai2785@gmail.com>
2024-01-06 17:32:51 +0545

    add comments to index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const limit = 20;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// function to retrieve only top rated animes
 const getTopRated = async (offset) => {
   try {
     const response = await axios.get(
@@ -23,6 +24,7 @@ const getTopRated = async (offset) => {
   }
 };
 
+// function to retrieve searched animes
 const getSearchedData = async (offset, limit, query) => {
   try {
     const response = await axios.get(
@@ -37,7 +39,10 @@ const getSearchedData = async (offset, limit, query) => {
 
 app.get("/", async (req, res) => {
   try {
+    // retrieving trending animes
     const trendingResponse = await axios.get(`${API_URL}/trending/anime`);
+
+    // retrieving default 10 top rated animes
     const topRatedResponse = await axios.get(
       `${API_URL}/anime?sort=-averageRating`
     );
@@ -55,7 +60,10 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/top-rated", async (req, res) => {
+  // getting page value, first setting initial to zero if there is no value of page
   const page = parseInt(req.query.page) || 0;
+
+  // setting the offset to retrieve from first page
   const offset = page * limit;
 
   const topAnimes = await getTopRated(offset);
@@ -69,14 +77,17 @@ app.get("/top-rated", async (req, res) => {
 });
 
 app.get("/anime-detail", async (req, res) => {
+  // retrieving id of anime to display its details
   const id = req.query.id;
 
+  // retrieve specifi anime using id
   const response = await axios.get(`${API_URL}/anime/${id}`);
 
   res.render("anime-detail.ejs", { anime: response.data.data });
 });
 
 app.get("/search", async (req, res) => {
+  // retrieving search text to be displayed
   const query = req.query.search;
   const page = parseInt(req.query.page) || 0;
   const offset = page * limit;
